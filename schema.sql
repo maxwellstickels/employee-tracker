95424d495eae325936db0e9dfb3594482e4816bb
Max Stickels <maxwellstickels@gmail.com>
2021-06-01 21:09:05 -0700

    Initial database set up created w/ schema.sql

---
+++ schema.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+USE employees_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL(10, 2),
+    department_id INT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name DECIMAL(10, 2),
+    role_id INT,
+    manager_id INT,
+    primary key (id)
+);
